a4c0e868ccc373f97426fa31140819cdf814c15d
Paulo Sergio <paulosergioilha38@gmail.com>
2022-12-31 17:29:39 -0300

    update cards

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,20 +1,16 @@
 import "./styles.css";
 import carImg from "../../assets/car.png";
+import Button from "../Button";
 
 export default function Card() {
   return (
-    <div className="ct-card ct-mb20">
-      <div className="ct-title-card">
-        <img src={carImg} alt="Car" />
-        <h2>Audi Supra TT</h2>
-      </div>
+    <div className="ct-card">
+      <img src={carImg} alt="Carro" />
+      <h3>Audi Supra TT</h3>
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate,
-        nisi
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate, nisi
       </p>
-      <div className="ct-aligned-card">
-        <div className="ct-buy-button">Comprar</div>
-      </div>
+      <Button />
     </div>
   );
 }
